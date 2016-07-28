b631e6559406eccd8cf637ffab099bd03ca2bd55
AhmedBadreldin <ahmedbadreldin@learniosfreecoms-mac.local>
2016-07-28 22:06:22 +0100

    Initial Commit

---
+++ ProfilePgTests/ProfilePgTests.swift
@@ -0,0 +1,36 @@
+//
+//  ProfilePgTests.swift
+//  ProfilePgTests
+//
+//  Created by Ahmed Badreldin on 7/28/16.
+//  Copyright © 2016 Ahmed Badreldin. All rights reserved.
+//
+
+import XCTest
+@testable import ProfilePg
+
+class ProfilePgTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
