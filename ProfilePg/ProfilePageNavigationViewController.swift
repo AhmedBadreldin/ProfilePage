47074f8e5b160b34bb79ae6b56da905faf9d3333
AhmedBadreldin <asb5495@psu.edu>
2016-08-08 15:16:02 +0100

    ProfilePage - Ahmed 8/8/2016

---
+++ ProfilePg/ProfilePageNavigationViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ProfilePageNavigationViewController.swift
+//  ProfilePg
+//
+//  Created by Ahmed Badreldin on 8/8/16.
+//  Copyright © 2016 Ahmed Badreldin. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class NavigationViewController: UINavigationController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func preferredStatusBarStyle() -> UIStatusBarStyle {
+        return UIStatusBarStyle.LightContent
+    }
+}
